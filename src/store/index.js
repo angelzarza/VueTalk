552258b8ac0c6476e13d1de40d89f67c0eddf5e6
Angel <anzo3d@gmail.com>
2021-10-15 18:41:33 +0200

    Config Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import messages from "./messages";
+import rooms from "./rooms";
+import user from "./user";
+import utils from "./utils";
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
-  modules: {},
+  actions: {
+    checkAuht() {},
+  },
+  modules: {
+    messages,
+    rooms,
+    user,
+    utils,
+  },
 });
+
+export default store;
+
+// Initial load
+this.$store.dispatch("checkAuth");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,49 @@
 <template>
-  <div></div>
+  <div class="home">
+    <section class="section">
+      <div class="container has-text-centered">
+        <button v-if="!user" @click="doLogin" class="button">
+          Login with Google 😄
+        </button>
+        <template v-else>
+          <h1 class="title has-text-centered">Hi {{ user.displayName }}</h1>
+          <button @click="doLogout" class="button">Logout 👋</button>
+        </template>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import { fb, auth } from "../firebase";
+
 export default {
   name: "Home",
+
+  data() {
+    return {
+      user: null,
+    };
+  },
+
+  methods: {
+    async doLogin() {
+      try {
+        const provider = new fb.auth.GoogleAuthProvider();
+        const user = await auth.signInWithPopup(provider);
+        this.user = user.user;
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+    async doLogout() {
+      try {
+        await auth.signOut();
+        this.user = null;
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+  },
 };
 </script>
